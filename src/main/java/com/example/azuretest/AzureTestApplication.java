72f4283faaa01d9d3dfd5d2680cb342e53dc7cce
Maren Lilleberre <marenlilleberre@gmail.com>
2023-01-13 10:27:14 +0100

    add datasource and mapping for db

--- src/main/java/com/example/azuretest/AzureTestApplication.java
+++ src/main/java/com/example/azuretest/AzureTestApplication.java
@@ -1,5 +1,6 @@
 package com.example.azuretest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @RestController
 public class AzureTestApplication {
+
+	final
+	DataSource dataSource;
+
+	public AzureTestApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@GetMapping("/")
 	public String helloWorld(@RequestParam(defaultValue = "Cloud") String name) {
 		return "Hello " + name;
@@ -18,6 +28,11 @@ public class AzureTestApplication {
 	@GetMapping("/user-agent")
 	public String userAgent(HttpServletRequest request) {
 		return request.getHeader("User-Agent");
+	}
+	@GetMapping("/db")
+	public String getDatabase(){
+		return "SELECT 1";
+
 	}
 
 	public static void main(String[] args) {
